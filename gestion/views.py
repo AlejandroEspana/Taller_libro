cc62cdf389c86a0a78c5fa6fbe20c14a0d9f06c5
Luciana-Cuenca <luciana.cuenca@campusucc.edu.co>
2026-04-23 09:11:59 -0500

    creacion eliminacion

--- gestion/views.py
+++ gestion/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import redirect, render
-
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Autor, Libro
 
 
@@ -15,3 +14,25 @@ def lista_autores(request):
 def lista_libros(request):
     libros = Libro.objects.select_related('autor').all().order_by('titulo')
     return render(request, 'gestion/lista_libros.html', {'libros': libros})
+
+
+#Eliminar autor
+def eliminar_autor(request, pk):
+    autor = get_object_or_404(Autor, pk=pk)
+
+    if request.method == 'POST':
+        autor.delete()
+        return redirect('lista_autores')
+
+    return render(request, 'gestion/autor_confirm_delete.html', {'autor': autor})
+
+
+#Eliminar libro
+def eliminar_libro(request, pk):
+    libro = get_object_or_404(Libro, pk=pk)
+
+    if request.method == 'POST':
+        libro.delete()
+        return redirect('lista_libros')
+
+    return render(request, 'gestion/libro_confirm_delete.html', {'libro': libro})
